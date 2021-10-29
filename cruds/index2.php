73c0276a1e7997e071548cf8ba0a0edda2a47cc8
Raju Kumar <rakumar@csumb.edu>
2021-10-28 17:09:58 -0700

    updated indexes files

--- cruds/index2.php
+++ cruds/index2.php
@@ -11,12 +11,11 @@
     <meta charset="UTF-8">
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css"
           integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="app.css" rel="stylesheet"/>
 
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Products Page</title>
 </head>
     <body>
